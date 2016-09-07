f3220df1d509ff711ea59add4b7359ac035658d8
Tai Le <lylienster@gmail.com>
2016-09-06 23:24:35 -0500

    Use JQuery to manipulate the site

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -1,13 +1,28 @@
 ﻿(function () {
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Tyler Le";
+    //var ele = $("#username");
+    //ele.innerHTML = "Tyler Le";
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function () {
-        main.style.background = "#888";
-    }
+    //var main = document.getElementById("main");
+    //main.onmouseenter = function () {
+    //    main.style.background = "#888";
+    //}
 
-    main.onmouseleave = function () {
-        main.style.background = "";
-    }
-})()
+    //main.onmouseleave = function () {
+    //    main.style.background = "";
+    //}
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        }
+        else {
+            $(this).text("Hide Sidebar");
+        }
+
+    });
+
+})();
