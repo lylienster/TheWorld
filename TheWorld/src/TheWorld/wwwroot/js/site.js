37475886bf3fa6dafd68dbf9a7865acead5517e0
Tai Le <lylienster@gmail.com>
2016-09-06 22:38:19 -0500

    Create site.js file. Create bower.js for managing client side dependencies. Install Underscore and JQuery using Bower

---
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    var ele = document.getElementById("username");
+    ele.innerHTML = "Tyler Le";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style.background = "#888";
+    }
+
+    main.onmouseleave = function () {
+        main.style.background = "";
+    }
+})()
