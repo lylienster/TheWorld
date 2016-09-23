8d8bc3fb919b19a3b385c51188405005e154e9cf
Tai Le <lylienster@gmail.com>
2016-09-22 22:48:40 -0500

    Create form and add validations

--- TheWorld/src/TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/src/TheWorld/wwwroot/js/tripsController.js
@@ -19,7 +19,14 @@
         {
             name: "World Trip",
             created: new Date()
-        }]
+        }];
+
+        vm.newTrip = {};
+
+        vm.addTrip = function () {
+            vm.trips.push({ name: vm.newTrip.name, created: new Date() });
+            vm.newTrip = {};
+        }
 
     }
 })();
