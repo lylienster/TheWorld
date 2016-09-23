e9d9ef4e9b0135eb44bb23765621ad33adb7a6bb
Tai Le <lylienster@gmail.com>
2016-09-22 23:06:02 -0500

    Retrieve data from server with AngularJS

--- TheWorld/src/TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/src/TheWorld/wwwroot/js/tripsController.js
@@ -6,22 +6,28 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
         
         var vm = this;
 
-        vm.name = "Tai";
-
-        vm.trips = [{
-            name: "US Trip",
-            created: new Date()
-        },
-        {
-            name: "World Trip",
-            created: new Date()
-        }];
+        vm.trips = []
 
         vm.newTrip = {};
+
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+            .then(function (response) {
+                //success
+                angular.copy(response.data, vm.trips);
+            }, function (error) {
+                //failure
+                vm.errorMessage = "Failed to load the data " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
 
         vm.addTrip = function () {
             vm.trips.push({ name: vm.newTrip.name, created: new Date() });
