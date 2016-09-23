bfa50e60461ad13688e5cb4b32a93c5b8adc1b50
Tai Le <lylienster@gmail.com>
2016-09-22 22:21:02 -0500

    Create first Angular Controller

---
+++ TheWorld/src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,15 @@
+﻿
+(function () {
+
+    "use strict";
+
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController() {
+        
+        var vm = this;
+
+        vm.name = "Tai";
+    }
+})();
