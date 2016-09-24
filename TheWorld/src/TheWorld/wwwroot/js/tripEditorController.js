b590b07243e42541317eb04d0fe3bea88f21875d
Tai Le <lylienster@gmail.com>
2016-09-24 15:47:56 -0500

    Implement tripEditorController and use it to call the api to get the stops

--- TheWorld/src/TheWorld/wwwroot/js/tripEditorController.js
+++ TheWorld/src/TheWorld/wwwroot/js/tripEditorController.js
@@ -4,8 +4,24 @@
     angular.module("app-trips")
         .controller("tripEditorController", tripEditorController);
 
-    function tripEditorController() {
+    function tripEditorController($routeParams, $http) {
         var vm = this;
+
+        vm.tripName = $routeParams.tripName;
+        vm.stops = [];
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips/" + vm.tripName + "/stops")
+            .then(function (response) {
+                // success
+                angular.copy(response.data, vm.stops);
+            }, function (error){
+                vm.errorMessage = "Failed to load stops";
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            })
 
     }
 }());
